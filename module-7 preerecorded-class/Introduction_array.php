709f354234509767210ec08ac29f9833b4d7a101
sumonpro-dev <sumonshriyan@gmail.com>
2024-10-20 19:56:18 +0600

    first commit preerecoded class intro array

---
+++ module-7 preerecorded-class/Introduction_array.php
@@ -0,0 +1,52 @@
+<?php
+
+// //simple veriable
+// $i = "Sumon";
+// echo $i;
+
+// //array
+// $students = [
+//     "Sumon",
+//     "Rakib",
+//     "Rakibul",
+//     "Sakib",
+//     "Sakibul",
+//     224991,
+//     1818852496,
+//     198513,
+//     "Parvin",
+//     "Sharmin",
+//     611898,
+//     1502172299
+// ];
+
+// echo $students[6];
+// var_dump($students);
+
+// //avabe dileo hobe array
+// $students2 = array(
+//     "Sumon", 
+//     "Rakib", 
+//     "Rakibul", 
+//     "Sakib", 
+//     "Sakibul", 
+//     224991, 
+//     1818852496, 
+//     198513, 
+//     "Parvin", 
+//     "Sharmin", 
+//     611898, 
+//     1502172299
+// );
+// var_dump($students2); //var_dump function
+// echo count($students2); //count function
+
+// $s = count($students2);
+// for ($i=0; $i < $s; $i++) { 
+//     echo $students2[$i]."\n";
+// }
+
+// for ($i=$s-1; $i > 0; $i--) { 
+//     echo $students2[$i]."\n";
+// }
+
